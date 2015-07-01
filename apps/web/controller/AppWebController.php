de0cc56606766fc9472a282922eb6d4e5652799b
rkinjo <rkinjo@crooz.co.jp>
2015-07-01 12:15:08 +0900

    first commit

---
+++ apps/web/controller/AppWebController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * web基底コントローラ
+ * 
+ * @version   2015/02/19 -
+ * @author    -
+ * @since     2015/02/19
+ * @copyright (C)2015 CROOZ All Rights Reserved
+ */
+class AppWebController extends CroozController {
+	/**
+	 * ここに処理を追加してください
+	 * 
+	 */
+	public function applicationFilter() {
+	}
+}
